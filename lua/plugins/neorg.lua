7048ce8ae806adc3fdf1b6d479a465c9f565abe5
mohamedhw <mohamedhw2000@gmail.com>
2025-03-09 15:07:58 +0200

    feat:added the norg for notes

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    version = "*", -- pins to the latest stable release; change if needed
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviors
+          ["core.concealer"] = {}, -- Adds icons to your documents
+          ["core.ui.calendar"] = {},
+          ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+          ["core.integrations.nvim-cmp"] = {},
+          ["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
+          ["core.qol.toc"] = {},
+          ["core.qol.todo_items"] = {},
+          ["core.looking-glass"] = {},
+          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+          ["core.export"] = {},
+          ["core.export.markdown"] = { config = { extensions = "all" } },
+          ["core.summary"] = {},
+          ["core.tangle"] = { config = { report_on_empty = false } },
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes/notes", -- change path as needed
+                work = "~/notes/work",
+              },
+              default_workspace = "work",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
